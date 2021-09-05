2317ef839db369270274baa04983c09de7254b9f
Arulselvan Madhavan <arulselvan1234@gmail.com>
2021-09-04 20:46:04 -0700

    Save

--- mmul-harness-instructional/dgemm-blocked.cpp
+++ mmul-harness-instructional/dgemm-blocked.cpp
@@ -57,45 +57,42 @@ void write_block(int start, double *src, double *dest, int b, int n) {
 
 void square_dgemm_blocked(int n, int block_size, double *A, double *B,
                           double *C) {
-  // insert your code here
+
   int Nb = n / block_size;
-  // TODO: Single block_buf
-  std::vector<double> a_block_buf(block_size * block_size);
-  std::vector<double> b_block_buf(block_size * block_size);
-  std::vector<double> c_block_buf(block_size * block_size);
+
+  std::vector<double> block_buf(3 * block_size * block_size);
+  double *AA = block_buf.data() + 0;
+  double *BB = AA + block_size * block_size;
+  double *CC = BB + block_size * block_size;
+
   for (int i = 0; i < Nb; i++) {
     for (int j = 0; j < Nb; j++) {
 
       int cpos = get_start(i, j, block_size, n);
-      copy_block(cpos, C, c_block_buf.data(), block_size, n);
+      copy_block(cpos, C, CC, block_size, n);
       for (int k = 0; k < Nb; k++) {
-        // std::cout << i << "\t" << k << "block\n";
-        // std::cout << k << "\t" << j << "block\n";
-
         int apos = get_start(i, k, block_size, n);
         int bpos = get_start(k, j, block_size, n);
         // std::cout << apos << "\t" << bpos << "\n";
-        copy_block(apos, A, a_block_buf.data(), block_size, n);
-        copy_block(bpos, B, b_block_buf.data(), block_size, n);
+        copy_block(apos, A, AA, block_size, n);
+        copy_block(bpos, B, BB, block_size, n);
 
         // Basic matrix mul on block
         for (int row_id = 0; row_id < block_size; row_id++) {
           for (int col_id = 0; col_id < block_size; col_id++) {
             int out_idx = vec_idx(row_id, col_id, block_size);
+            double c_cout = CC[out_idx];
             for (int m = 0; m < block_size; m++) {
               int left_idx = vec_idx(row_id, m, block_size);
               int right_idx = col_iter(col_id, m, block_size);
-              // std::cout << a_block_buf[left_idx] << " * "
-              //           << b_block_buf[right_idx] << "\n";
-              c_block_buf[out_idx] =
-                  c_block_buf[out_idx] +
-                  (a_block_buf[left_idx] * b_block_buf[right_idx]);
+              c_cout = c_cout + (AA[left_idx] * BB[right_idx]);
             }
+            CC[out_idx] = c_cout;
           }
         }
       }
       // Write result back
-      write_block(cpos, c_block_buf.data(), C, block_size, n);
+      write_block(cpos, CC, C, block_size, n);
     }
   }
 }
