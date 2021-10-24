91c85ed7b258193cc0138fac2f60fb0aff05c59e
Arulselvan Madhavan <arulselvan1234@gmail.com>
2021-10-23 20:17:20 -0700

    Save all

--- HW5/scripts/skeleton-gpu-batch-script.sh
+++ HW5/scripts/skeleton-gpu-batch-script.sh
@@ -15,8 +15,20 @@ export LIBOMPTARGET_INFO=4
 # it is assumed your environment is set up properly for using the Cori GPUs
 # prior to you launching this batch script
 #
-nvprof -m sm_efficiency ./sobel_gpu
+for b in 1 4 16 64 256 1024 4096
+do
+    for t in 32 64 128 256 512 1024
+    do
+	export NUM_BLOCKS=$b
+	export THREADS_PER_BLOCK=256
+	echo "Running B=${b} T=${t}"
+	nvprof -m sm_efficiency --csv --log-file ../logs/gpu/eff/sobel_gpu_eff_${b}_${t}.txt ./sobel_gpu
+	nvprof --csv --log-file ../logs/gpu/exec/sobel_gpu_exec_${b}_${t}.txt ./sobel_gpu
+    done
+done
 
-nvprof -m sm_efficiency ./sobel_cpu_omp_offload
+# nvprof -m sm_efficiency ./sobel_gpu
+
+# nvprof -m sm_efficiency ./sobel_cpu_omp_offload
 
 
